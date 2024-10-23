30bb509d61ad6d588ef4954cc1605249371603ec
Will Velida <willvelida@hotmail.co.uk>
2024-10-23 19:18:26 +1100

    Removing json output property

--- modules/aks-automatic-cluster/outputs.tf
+++ modules/aks-automatic-cluster/outputs.tf
@@ -7,7 +7,3 @@ output "cluster_id" {
   description = "The Id of the deployed AKS Automatic Cluster"
   value       = azapi_resource.aks.id
 }
-
-output "aks_json_output" {
-  value = jsondecode(azapi_resource.aks.output)
-}
