881942335fe36d0366783cb8c57f84a14b3583f1
Will Velida <willvelida@hotmail.co.uk>
2024-10-26 14:10:20 +1100

    Adding enable auto scaling flag
    Signed-off-by: Will Velida <willvelida@hotmail.co.uk>

--- modules/aks-cluster/main.tf
+++ modules/aks-cluster/main.tf
@@ -10,8 +10,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
     name = "default"
     node_count = var.node_count
     vm_size = var.vm_size
+    enable_auto_scaling = true
     type = "VirtualMachineScaleSets"
-    auto_scaling_enabled = true
+    
     min_count = var.min_count
     max_count = var.max_count
   }
